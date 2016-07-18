979a986469646dfbcc39d0f5fd3fac2fbdaca4d7
Keon Kim <kwk236@gmail.com>
2016-07-18 22:08:49 +0900

    use boost format instead of plain formatting

--- src/mlpack/methods/preprocess/preprocess_describe_main.cpp
+++ src/mlpack/methods/preprocess/preprocess_describe_main.cpp
@@ -5,10 +5,14 @@
  * Descriptive Statistics Class and CLI executable.
  */
 #include <mlpack/core.hpp>
+#include <mlpack/core/data/statistics.hpp>
+
+#include <boost/format.hpp>
 
 using namespace mlpack;
 using namespace mlpack::data;
 using namespace std;
+using namespace boost;
 
 PROGRAM_INFO("Descriptive Statistics", "This utility takes a dataset prints "
     "out the statistical facts about the data.");
@@ -17,108 +21,6 @@ PROGRAM_INFO("Descriptive Statistics", "This utility takes a dataset prints "
 PARAM_STRING_REQ("input_file", "File containing data,", "i");
 PARAM_INT("dimension", "Dimension of the data", "d", 0);
 PARAM_INT("precision", "preferred precision of the result", "p", 2);
-
-// Statistics class, it calculates most of the statistical elements in its
-// constructor.
-template <typename T>
-class Statistics
-{
- public:
-  Statistics(arma::Mat<T> input, size_t norm_type = 1, bool columnMajor = true):
-      data(input)
-  {
-    minVec = arma::min(data, columnMajor);
-    maxVec = arma::max(data, columnMajor);
-    meanVec = arma::mean(data, columnMajor);
-    medianVec = arma::median(data, columnMajor);
-    stdVec = arma::stddev(data, 1, columnMajor);
-    varVec = arma::var(data, 1, columnMajor);
-  }
-  double Min(const size_t dimension) const
-  {
-    return minVec(dimension);
-  }
-
-  double Max(const size_t dimension) const
-  {
-    return maxVec(dimension);
-  }
-
-  double Range(const size_t dimension) const
-  {
-    return maxVec(dimension) - minVec(dimension);
-  }
-
-  double Mean(const size_t dimension) const
-  {
-    return meanVec(dimension);
-  }
-
-  double Median(const size_t dimension) const
-  {
-    return medianVec(dimension);
-  }
-
-  double Variance(const size_t dimension) const
-  {
-    return varVec(dimension);
-  }
-
-  double StandardDeviation(const size_t dimension) const
-  {
-    return stdVec(dimension);
-  }
-
-  double Skewness(const size_t dimension) const
-  {
-    return this->CentralMoment(3, dimension);
-  }
-
-  double Kurtosis(const size_t dimension) const
-  {
-    return this->CentralMoment(4, dimension);
-  }
-
-  double RawMoment(const size_t order, const size_t dimension) const
-  {
-    // E(x)^order
-    double moment = 0;
-    for (size_t i = 0; i < data.n_cols; ++i)
-    {
-      moment += pow(data(dimension, i), order);
-    }
-    return moment / data.n_cols;
-  }
-
-  double CentralMoment(const size_t order, const size_t dimension) const
-  {
-    // E(X-u)^order
-    if (order == 1)
-    {
-      return 0.0;
-    }
-    double moment = 0;
-    for (size_t i = 0; i < data.n_cols; ++i)
-    {
-      moment += pow(data(dimension, i) - meanVec(dimension), order);
-    }
-    return moment / data.n_cols;
-  }
-
-  double StandardError(const size_t dimension) const
-  {
-     return stdVec(dimension) / sqrt(data.n_cols);
-  }
- private:
-  arma::Mat<T> data;
-
-  arma::vec minVec;
-  arma::vec maxVec;
-  arma::vec meanVec;
-  arma::vec medianVec;
-  arma::vec stdVec;
-  arma::vec varVec;
-};
 
 /**
  * Make sure a CSV is loaded correctly.
@@ -138,60 +40,57 @@ int main(int argc, char** argv)
   Statistics<double> stats(data);
 
   // Headers
-  Log::Info << left << setw(8) << "dim";
-  Log::Info << left << setw(8) << "var";
-  Log::Info << left << setw(8) << "mean";
-  Log::Info << left << setw(8) << "std";
-  Log::Info << left << setw(8) << "median";
-  Log::Info << left << setw(8) << "min";
-  Log::Info << left << setw(8) << "max";
-  Log::Info << left << setw(8) << "range";
-  Log::Info << left << setw(10) << "skewness";
-  Log::Info << left << setw(10) << "kurtosis";
-  Log::Info << left << setw(10) << "SE";
-  Log::Info << endl;
+  Log::Info << boost::format("%-s	%-s	%-s	%-s	%-s	%-s	%-s	%-s	%-s	%-s	%-s")
+      % "dim"
+      % "var"
+      % "mean"
+      % "std"
+      % "median"
+      % "min"
+      % "max"
+      % "range"
+      % "skew"
+      % "kurt"
+      % "SE"
+      << endl;
 
   // If the user specified dimension, describe statistics of the given
   // dimension. If it dimension not specified, describe all dimensions.
   if (CLI::HasParam("dimension"))
   {
-      // Options
-      Log::Info << fixed;
-      Log::Info << setprecision(2);
-      // Describe Data
-      Log::Info << left << setw(8) << dimension;
-      Log::Info << left << setw(8) << stats.Variance(dimension);
-      Log::Info << left << setw(8) << stats.Mean(dimension);
-      Log::Info << left << setw(8) << stats.StandardDeviation(dimension);
-      Log::Info << left << setw(8) << stats.Median(dimension);
-      Log::Info << left << setw(8) << stats.Min(dimension);
-      Log::Info << left << setw(8) << stats.Max(dimension);
-      Log::Info << left << setw(8) << stats.Range(dimension);
-      Log::Info << left << setw(10) << stats.Skewness(dimension);
-      Log::Info << left << setw(10) << stats.Kurtosis(dimension);
-      Log::Info << left << setw(10) << stats.StandardError(dimension);
-      Log::Info << endl;
+    Log::Info << boost::format("%-6i	%-.4f	%-.4f	%-.4f	%-.4f	%-.4f	%-.4f	%-.4f"
+        "	%-.4f	%-.4f	%-.4f")
+        % dimension
+        % stats.Variance(dimension)
+        % stats.Mean(dimension)
+        % stats.StandardDeviation(dimension)
+        % stats.Median(dimension)
+        % stats.Min(dimension)
+        % stats.Max(dimension)
+        % stats.Range(dimension)
+        % stats.Skewness(dimension)
+        % stats.Kurtosis(dimension)
+        % stats.StandardError(dimension)
+        << endl;
   }
   else
   {
     for (size_t i = 0; i < data.n_rows; ++i)
     {
-      // Options
-      Log::Info << fixed;
-      Log::Info << setprecision(2);
-      // Describe Data
-      Log::Info << left << setw(8) << i;
-      Log::Info << left << setw(8) << stats.Variance(i);
-      Log::Info << left << setw(8) << stats.Mean(i);
-      Log::Info << left << setw(8) << stats.StandardDeviation(i);
-      Log::Info << left << setw(8) << stats.Median(i);
-      Log::Info << left << setw(8) << stats.Min(i);
-      Log::Info << left << setw(8) << stats.Max(i);
-      Log::Info << left << setw(8) << stats.Range(i);
-      Log::Info << left << setw(10) << stats.Skewness(i);
-      Log::Info << left << setw(10) << stats.Kurtosis(i);
-      Log::Info << left << setw(10) << stats.StandardError(i);
-      Log::Info << endl;
+      Log::Info << boost::format("%-6i	%-.4f	%-.4f	%-.4f	%-.4f	%-.4f	%-.4f	"
+          "%-.4f	%-.4f	%-.4f	%-.4f")
+          % i
+          % stats.Variance(i)
+          % stats.Mean(i)
+          % stats.StandardDeviation(i)
+          % stats.Median(i)
+          % stats.Min(i)
+          % stats.Max(i)
+          % stats.Range(i)
+          % stats.Skewness(i)
+          % stats.Kurtosis(i)
+          % stats.StandardError(i)
+          << endl;
     }
   }
 }
