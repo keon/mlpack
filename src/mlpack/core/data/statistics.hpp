6280889639ff33617c5393ad63aac8b8c50c805f
Keon Kim <kwk236@gmail.com>
2016-07-21 23:10:01 +0900

    add proper comments for statistics functions

--- src/mlpack/core/data/statistics.hpp
+++ src/mlpack/core/data/statistics.hpp
@@ -21,89 +21,102 @@ class Statistics
  public:
 
   /**
-   * StandardError of Mean
+   * Create the Statistics object with the given input and options.
    *
-   * @param dimension Dimension
-   * @return sum of nth power deviations
+   * @param input Dataset a user wishes to analyze.
+   * @param population Determines if the input is considered sample or
+   *        population data.
+   * @param columnMajor Determines if the input is columnMajor or not.
    */
   Statistics(const arma::Mat<T>& input, const bool population = false,
       const bool columnMajor = true);
 
   /**
-   * StandardError of Mean
+   * Calculates minimum of the input matrix with the given dimension.
    *
    * @param dimension Dimension
-   * @return sum of nth power deviations
+   * @return Minimum value of the given dimension.
    */
   double Min(const size_t dimension) const;
 
   /**
-   * StandardError of Mean
+   * Calculates maximum of the input matrix with the given dimension.
    *
    * @param dimension Dimension
-   * @return sum of nth power deviations
+   * @return Maximum value of the given dimension.
    */
   double Max(const size_t dimension) const;
 
-  std::pair<double, double> MinMax(const size_t dimension) const;
   /**
-   * StandardError of Mean
+   * Calculates minimum and maximum of the input matrix with the given
+   * dimension. It is equivalent function as std::minmax, but it applies
+   * to the armadillo matrix inputs.
    *
    * @param dimension Dimension
-   * @return sum of nth power deviations
+   * @return A pair of minimum and maximum values of the given dimension.
+   */
+  std::pair<double, double> MinMax(const size_t dimension) const;
+
+  /**
+   * Calculates minimum and maximum and uses those values to derive range of
+   * the dataset of the given dimension.
+   *
+   * @param dimension Dimension
+   * @return Range value of the given dimension.
    */
   double Range(const size_t dimension) const;
 
   /**
-   * Mean, also known as average.
+   * Calculates mean, also known as average, of the given dimension of the input
+   * matrix.
    *
    * @param dimension Dimension
-   * @return sum of nth power deviations
+   * @return Mean value of the given dimension.
    */
   double Mean(const size_t dimension) const;
 
   /**
-   * StandardError of Mean
+   * Calculates median of the given dimension of the input matrix.
    *
    * @param dimension Dimension
-   * @return sum of nth power deviations
+   * @return Median of the given dimension.
    */
   double Median(const size_t dimension) const;
 
   /**
-   * StandardError of Mean
+   * Calculates varianc e of the given dimension of the input matrix.
    *
    * @param dimension Dimension
-   * @return sum of nth power deviations
+   * @return Variance value of the given dimension.
    */
   double Variance(const size_t dimension) const;
 
   /**
-   * StandardError of Mean
+   * Calculates standard deviation of the given dimension.
    *
    * @param dimension Dimension
-   * @return sum of nth power deviations
+   * @return Standard deviation.
    */
   double StandardDeviation(const size_t dimension) const;
 
   /**
-   * StandardError of Mean
+   * Calculates Skewness of the given dimension.
    *
    * @param dimension Dimension
-   * @return sum of nth power deviations
+   * @return Skewness
    */
   double Skewness(const size_t dimension) const;
 
   /**
-   * The sum of deviations to the Nth Power
+   * Calculates kurtosis of the given dimension.
    *
    * @param n power
-   * @return sum of nth power deviations
+   * @return Kurtosis.
    */
   double Kurtosis(const size_t dimension) const;
 
   /**
-   * The sum of deviations to the Nth Power
+   * Calculates the sum of deviations to the Nth Power
    *
    * @param n power
    * @param dimension Dimension
@@ -112,10 +125,10 @@ class Statistics
   double SumNthPowerDeviations(const size_t n, const size_t dimension) const;
 
   /**
-   * StandardError of Mean
+   * Calculates standard error.
    *
    * @param dimension Dimension
-   * @return sum of nth power deviations
+   * @return Standard error.
    */
   double StandardError(const size_t dimension) const;
 
